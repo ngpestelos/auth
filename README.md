1d9eb541eb4cf16a84ec3428baaa387cd92a9135
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-09-12 14:38:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Based on the [Railscasts](http://railscasts.com) episode [Authentication from Scratch](http://railscasts.com/episodes/250-authentication-from-scratch).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Auth::Application.config.secret_key_base = '011384649005bb2f8ab91e179aba6af78076b5304df8b54e2b705c179224176e152dc459eeddcbc90af38ba5eb5a791cc16d8956ff878513bb81f5cdd83e7cdf'
